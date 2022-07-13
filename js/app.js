4466b058dda22ea09a27d7662a7bc4524d47e693
gonzaaliano <gonzaaliano@hotmail.com>
2022-07-12 21:18:58 -0300

    Validación formulario

--- js/app.js
+++ js/app.js
@@ -1,4 +1,53 @@
-window.addEventListener("scroll", function(){
+// Scroll Navbar
+
+window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+
+
+// Email JS
+function validate() {
+    let name = document.querySelector(".name");
+    let email = document.querySelector(".email");
+    let message = document.querySelector(".message");
+    let sendBtn = document.querySelector(".send-btn");
+
+    sendBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (name.value == "" || email.value == "" || message.value == "") {
+            emptyerror();
+
+        } else {
+            sendmail(name.value, email.value, message.value);
+            success();
+        }
+
+    })
+}
+
+validate();
+
+function sendmail(name, email, message) {
+    emailjs.send("service_p9x17ch", "template_i3splii", {
+        from_name: email,
+        to_name: name,
+        message: message,
+    });
+}
+
+function emptyerror() {
+    swal({
+        title: "Oh no!",
+        text: "Fields cannot be empty!",
+        icon: "error",
+    });
+}
+
+function success() {
+    swal({
+        title: "Email sent successfully",
+        icon: "success",
+    });
+}
