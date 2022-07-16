68b8ecf2f81462b737440cc479095b111c49d5a9
gonzaaliano <gonzaaliano@hotmail.com>
2022-07-16 00:20:01 -0300

    Intersection Observer

--- js/app.js
+++ js/app.js
@@ -1,9 +1,28 @@
 // Scroll Navbar
 
-window.addEventListener("scroll", function () {
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
-})
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".banner");
+
+const sectionOneOptions = {
+    rootMargin: "-150px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function (
+    entries, 
+    sectionOneObserver
+    ) {
+        entries.forEach(entry => {
+            if(!entry.isIntersecting) {
+                header.classList.add("nav-scrolled");
+            } else {
+                header.classList.remove("nav-scrolled");
+            }
+        })
+}, 
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
 
 
 
